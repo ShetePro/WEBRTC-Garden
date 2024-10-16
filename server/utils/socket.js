d62930fcc036ceb8548c52e8e2ad07201e422e25
ShetePro <sheteprolin@gmail.com>
2024-10-16 10:24:57 +0800

    feat(server): 缓存第一次登陆的sokcetid 刷新页面不丢失信息

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -1,55 +1,50 @@
-// const { createServer } = require("http");
-// const { Server } = require("socket.io");
-// const { RtcRoom } = require('../room');
-// const fs = require('fs');
-// const { stringify } = require('./util.js');
 import { createServer } from "http";
-import {RtcRoom} from "../room.js";
-import {stringify} from "./util.js";
-import {Server} from "socket.io";
+import { RtcRoom } from "../room.js";
+import { stringify } from "./util.js";
+import { Server } from "socket.io";
 
 const roomList = [];
-const userRoom = {};
-export function initSocket(app) {
-  const pemPath = process.cwd() + "\\bin\\";
-
-  const options = {
-    // key: fs.readFileSync(pemPath + 'localhost+5-key.pem'),
-    // cert: fs.readFileSync(pemPath + 'localhost+5.pem'),
-  };
+const userMap = {};
+export function initSocket() {
+  // const pemPath = process.cwd() + "\\bin\\";
+  // const options = {
+  // key: fs.readFileSync(pemPath + 'localhost+5-key.pem'),
+  // cert: fs.readFileSync(pemPath + 'localhost+5.pem'),
+  // };
   const server = createServer();
   const io = new Server(server, {
     cors: {
       credentials: true,
       // 跨域过滤源
-      origin: "*",
+      origin: "*"
       // origin: ["http://localhost:8796", 'http://localhost:3100', 'http://localhost:8080', 'http://192.168.19.129:8080'],
-    },
+    }
   });
   io.on("connection", (socket) => {
     console.log("用户已连接socket");
     socket.on("disconnect", () => {
       //监听用户断开事件
-      console.log("用户" + socket.id + "断开连接");
+      console.log("用户" + userMap[socket.id] + "断开连接");
       // 清除 房间内的用户
       const roomId = socket.data.roomId;
       const room = roomList.find((item) => item.id === roomId);
-      room && room.exit(socket.id);
-      io.to(roomId).emit("exit", socket.id);
+      room && room.exit(userMap[socket.id]);
+      io.to(roomId).emit("exit", userMap[socket.id]);
       io.in(socket.id).disconnectSockets();
     });
     // 连接就调用登录
     //监听login事件 返回socket用户id
     socket.on("login", (data) => {
-      console.log("用户" + socket.id + "登录");
-      socket.emit("login", socket.id);
+      userMap[socket.id] = data || socket.id;
+      socket.emit("login", userMap[socket.id]);
+      console.log("用户" + userMap[socket.id] + "登录", data);
     });
-    socket.on("createRoom", (data) => {
+    socket.on("createRoom", () => {
       console.log("创建房间");
       const room = new RtcRoom({
         id: Math.floor(Math.random() * 10000000),
-        createUser: socket.id,
-        roomSize: 10,
+        createUser: userMap[socket.id],
+        roomSize: 10
       });
       roomList.push(room);
       // 加入指定房间
@@ -60,15 +55,17 @@ export function initSocket(app) {
       let room;
       if (roomId) {
         room = roomList.find((item) => Number(item.id) === Number(roomId));
+        console.log("join", room);
         if (room) {
-          room.join(socket.id) && (flag = true);
+          room.join(userMap[socket.id]) && (flag = true);
         }
       }
       // 给对应房间 推送事件
       if (flag) {
+        console.log(userMap[socket.id]);
         io.in(socket.id).socketsJoin(roomId);
         io.to(roomId).emit("roomChange", stringify(room.getRoomData()));
-        io.to(roomId).emit("addUser", socket.id);
+        io.to(roomId).emit("addUser", userMap[socket.id]);
         // 保存进入的房间id
         socket.data.roomId = roomId;
       }
@@ -77,8 +74,8 @@ export function initSocket(app) {
         "join",
         stringify({
           success: flag,
-          data: room ? room.getRoomData() : {},
-        }),
+          data: room ? room.getRoomData() : {}
+        })
       );
     });
     socket.on("exit", (roomId) => {
@@ -87,14 +84,14 @@ export function initSocket(app) {
       if (roomId) {
         room = roomList.find((item) => Number(item.id) === Number(roomId));
         if (room) {
-          room.exit(socket.id) && (flag = true);
+          room.exit(userMap[socket.id]) && (flag = true);
         }
       }
       // 给对应房间 推送事件
       if (flag) {
         socket.leave(roomId);
         io.to(roomId).emit("roomChange", stringify(room.getRoomData()));
-        io.to(roomId).emit("exit", socket.id);
+        io.to(roomId).emit("exit", userMap[socket.id]);
       }
       console.log("离开房间", socket.rooms);
     });
@@ -114,8 +111,8 @@ export function initSocket(app) {
       const roomId = socket.data.roomId;
       const message = {
         msg: data,
-        userId: socket.id,
-        roomId,
+        userId: userMap[socket.id],
+        roomId
       };
       io.to(roomId).emit("message", stringify(message));
       const room = getRoom(socket);
